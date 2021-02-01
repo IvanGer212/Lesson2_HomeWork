1bfa7237919d6fb5886417e50d013fd3de34f4d2
IvanGer212 <78230937+IvanGer212@users.noreply.github.com>
2021-02-01 21:35:41 +0300

    Lesson 2 HomeWork (with Part 7)

--- src/GeekBrains/Main.java
+++ src/GeekBrains/Main.java
@@ -65,8 +65,14 @@ public class Main {
         //Задание 6:
         System.out.println("Задание 6:");
         int[] array6 = {2,7,3,1,5};
-        //findEqual(array6);
         System.out.println(findEqual(array6));
+
+        //Задание 7:
+        System.out.println("Задание 7:");
+        int[] array7 = {10,5,7,3,1,8,12,6};
+        byte n = -4;
+        shiftNum(array7,n);
+
 
 
 
@@ -153,6 +159,43 @@ public class Main {
                 }
             }
                return x;
+            }
+
+            // Метод сдвигает все члены массива на заданное число n влево (n-отрицательное) или вправо (n - положительное)
+            public static void shiftNum (int[] array, byte n) {
+                System.out.println(Arrays.toString(array));
+                int buff = 0;
+                int maxIndex = array.length - 1;                    // Переменная для определения максимального индекса в массиве
+                int count = 0;
+                while (count < Math.abs(n)) {                       // Будем запускать цикл сдвига на 1 позицию нужное нам количество раз (n)
+                    if (n > 0) {                                    // Если n - положительное
+                        for (int i = maxIndex; i > 0; i--) {        // Запускаем цикл чтобы сдвинуть на 1 место вправо
+                                                                    // Цикл идет от конца массива к началу
+                            buff = array[i];                        // В буфферную переменную записываем значение i-го члена
+                            if (i + 1 > maxIndex) {                 // Условие для перехода через конец массива в его начало
+                                array[i] = array[(maxIndex) - i];
+                                array[(maxIndex) - i] = buff;
+                            } else {
+                                array[i] = array[i + 1];            // Условие для членов массива, не переходящих через границу
+                                array[i + 1] = buff;
+                            }
+                        }
+                    } else if (n<0) {                               // Если n - отрицательное
+                        for (int i = 0; i<maxIndex; i++){           // Запускаем цикл чтобы сдвинуть на 1 место влево
+                                                                    // Цикл идет в прямом направлении от нулевого члена к последнему
+                            buff = array[i];
+                            if (i-1 <0) {                           // Условие для перехода через начало массива в его конец
+                                array[i] = array[maxIndex-i];
+                                array[maxIndex-i] = buff;
+                            } else {
+                                array[i] = array[i-1];              // Условие для членов массива, не переходящих через границу
+                                array[i-1] = buff;
+                            }
+                        }
+                    }
+                    count++;                                        // счетчик циклов
+                }
+                System.out.println(Arrays.toString(array));         // выводим конечный массив для самоконтроля
             }
 
 
